8c7bad54c7409ad157aee94b60b76681bf2e31a9
Mandy <144916639+louisa-mandy@users.noreply.github.com>
2024-05-19 21:58:03 +0700

    Update parking.java

--- parking.java
+++ parking.java
@@ -1,5 +1,6 @@
 package Parking_Ticket;
 
+import java.sql.SQLOutput;
 import java.util.LinkedList;
 import java.util.Scanner;
 
@@ -17,11 +18,36 @@ public class parking {
 
         if (login == 1) { // display the card balance depending the users account
             System.out.println(" Check Card Balance "); // we'll provide a default card balance, for ex Rp 50.000
+
+            System.out.println("Please Enter Your Username: ");
+            System.out.println(" ");
+            String usernameToCheck = mandy.next();
+            Double balance = SignIn.getUserBalanceByUsername(usernameToCheck);
+
+            if ( balance != null ) { // if balanca is true, print out user's balance
+                System.out.println(usernameToCheck+ "'s Balance: " + balance);
+            } else {
+                System.out.println("User not found, please try again");
+            }
+
+
         } else if (login == 2) {
             System.out.println(" Top Up Card Balance");
+            System.out.println(" ");
+            System.out.println("Please Enter Your Username: ");
+
+            String usernameToUpdate = mandy.next();
+
+            System.out.println("Enter your wanted balance: ");
+            double newBalance = mandy.nextDouble();
+
+            SignIn.updateUserBalance(usernameToUpdate, newBalance);
+
+
 
         } else if (login == 3) {
             System.out.println(" Pay Parking Fee ");
+
 
         } else {
             System.out.println("Please input a valid choice");
@@ -29,4 +55,6 @@ public class parking {
 
 
     }
+
+
 }
