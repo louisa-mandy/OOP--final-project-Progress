6804253c334ba7266a3fd88bd195b6b0e229ab4b
Mandy <144916639+louisa-mandy@users.noreply.github.com>
2024-05-24 20:01:29 +0700

    Update User.java

--- User.java
+++ User.java
@@ -1,49 +1,49 @@
 package Parking_Ticket;
 
 
-public class User {
-    // person, parkingcharge
-/*
-parkingChare(int chargePerHour, int totalhour, int totalcharge(chargeperhour * totalhour)) {
-Persom.PlateNumber("a", "b", 'money')
-total hour {
-person(saldo) - totalcharge
-}
 
-}
+public abstract class User {
 
-person : string Name, string PlateNumber, int money; int sisaSaldo
-*/
     private String email;
     private String password;
     private String username;
     private double balance;
+    private String userType;
 
-    public User(String email, String password, String username, double balance) {
-        this.email = email;
-        this.password = password;
-        this.username = username;
-        this.balance = balance;
+        public User(String email, String password, String username, double balance, String userType) {
+            this.email = email;
+            this.password = password;
+            this.username = username;
+            this.balance = balance;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public double getBalance() {
+            return balance;
+        }
+
+        public void setBalance(double balance) {
+            this.balance = balance;
+        }
+
+        public String getUserType() {
+            return userType;
+        }
+
     }
 
-    public String getEmail() {
-        return email;
-    }
 
-    public String getPassword() {
-        return password;
-    }
 
-    public String getUsername() {
-        return username;
-    }
 
-    public double getBalance() {
-        return balance;
-    }
 
-    public void setBalance(double balance) {
-        this.balance = balance;
-    }
-
-}
