f66ada3914a8c748ceba8d984e5a8295e4b2c9fa
Mandy <144916639+louisa-mandy@users.noreply.github.com>
2024-05-24 20:01:10 +0700

    Update SignIn.java

--- SignIn.java
+++ SignIn.java
@@ -3,6 +3,8 @@ package Parking_Ticket;
 import java.util.LinkedList;
 import java.util.Scanner;
 
+import java.util.InputMismatchException;
+
 public class SignIn {
 
     static LinkedList<String> Email = new LinkedList<String>(); // making these linkedlist static so it can be accessed in the main class
@@ -15,43 +17,62 @@ public class SignIn {
 
         Scanner mandy = new Scanner(System.in);  // Create a Scanner object
 
+        try {
+
+            // create a different user input? store the data to a linked list?
+            System.out.println(" ");
+
+            System.out.println("Sign In");
+
+            System.out.println("Enter Your Email :"); // data type string
+            String email = mandy.next();
+
+            System.out.println("Enter Your Password : "); // string
+            String password = mandy.next();
+
+            System.out.println("Enter Your Username : "); // string
+            String username = mandy.next();
+
+            System.out.println("Enter Your UserType ( Type in Regular or Premium ) : "); // string
+            String userType = mandy.next();
 
 
-        // create a different user input? store the data to a linked list?
-        System.out.println(" ");
 
-        System.out.println("Sign In");
+            //default balance
+            double initialBalance = 50;
 
-        System.out.println("Enter Your Email :"); // data type string
-        String email = mandy.next();
+            UserActions newUser;
+            if (userType.equalsIgnoreCase("Regular")) {
+                newUser = new RegularUser(email, password, username, initialBalance);
+            } else if (userType.equalsIgnoreCase("Premium")) {
+                newUser = new PremiumUser(email, password, username, initialBalance);
+            } else {
+                throw new IllegalArgumentException("Invalid user type.");
+            }
 
-        System.out.println("Enter Your Password : "); // string
-        String password = mandy.next();
+            users.add((User) newUser);
 
-        System.out.println("Enter Your Username : "); // string
-        String username = mandy.next();
-
-        //default balance
-        double initialBalance = 50;
-
-        //create a new user and ass to the users list
-        User newUser = new User (email, password, username,initialBalance);
+            Email.add(email);
+            Password.add(password);
+            Username.add(username); // adding the acc info inside each linked list
 
 
-        Email.add(email);
-        Password.add(password);
-        Username.add(username); // adding the acc info inside each linked list
-        users.add(newUser);
+            System.out.println("You have successfully created an account!");
+            System.out.println(" ");
+            System.out.println("Email : " + email); // displaying the acc info from the user input
+            System.out.println("Password : " + password);
+            System.out.println("Username : " + username);
+            System.out.println("Balance : $" + initialBalance);
+            System.out.println("UserType : " + userType);
+            System.out.println(" ");
+            System.out.println("Please proceed to the login option ");
 
-        System.out.println("You have successfully created an account!");
-        System.out.println(" ");
-        System.out.println("Email : " + email); // displaying the acc info from the user input
-        System.out.println("Password : " + password);
-        System.out.println("Username : " + username);
-        System.out.println("Balance : $" + initialBalance);
-        System.out.println(" ");
-        System.out.println("Please proceed to the login option ");
-
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input. Please enter a number for user type.");
+            mandy.next(); // Clear the invalid input
+        } catch (Exception e) {
+            System.out.println("An unexpected error occurred: " + e.getMessage());
+        }
 
     }
 
